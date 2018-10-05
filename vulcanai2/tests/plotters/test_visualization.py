753116cb41e86660cbf9e635c77dc1e671751176
rfratila <robertfratila10@gmail.com>
2018-10-05 18:38:11 -0400

    Add dnn saliency map tests

--- vulcanai2/tests/plotters/test_visualization.py
+++ vulcanai2/tests/plotters/test_visualization.py
@@ -33,9 +33,23 @@ class TestVisualization:
             num_classes=3
         )
 
-    def test_compute_saliency_map(self, cnn_class):
+    @pytest.fixture
+    def dnn_class(self):
+        from vulcanai2.models.dnn import DenseNet
+        return DenseNet(
+            name='Test_DenseNet_class',
+            dimensions=(200),
+            config={
+                'dense_units': [100],
+                'dropouts': [0.3],
+            },
+            num_classes=3
+        )
+
+    def test_compute_saliency_map_cnn(self, cnn_class):
         """
         Confirm hooks are removed, and gradient shape is the same as input.
+        For ConvNets.
         """
         test_input_1B = np.ones([1, *cnn_class.in_dim], dtype=np.float32)
         test_input_5B = np.ones([5, *cnn_class.in_dim], dtype=np.float32)
@@ -53,3 +67,25 @@ class TestVisualization:
 
         # Test hook removal
         assert cnn_class._backward_hooks == model_copy._backward_hooks
+
+    def test_compute_saliency_map_dnn(self, dnn_class):
+        """
+        Confirm hooks are removed, and gradient shape is the same as input.
+        For DenseNets.
+        """
+        test_input_1B = np.ones([1, dnn_class.in_dim], dtype=np.float32)
+        test_input_5B = np.ones([5, dnn_class.in_dim], dtype=np.float32)
+
+        model_copy = deepcopy(dnn_class)
+        # Test shape conservation
+        sal_map_1B = compute_saliency_map(
+            dnn_class,
+            test_input_1B, torch.tensor([2]))
+        assert sal_map_1B.shape == test_input_1B.shape
+        sal_map_5B = compute_saliency_map(
+            dnn_class, test_input_5B,
+            torch.tensor([0, 2, 1, 1, 0]))
+        assert sal_map_5B.shape == test_input_5B.shape
+
+        # Test hook removal
+        assert dnn_class._backward_hooks == model_copy._backward_hooks
