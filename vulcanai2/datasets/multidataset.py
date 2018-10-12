13db7c938789be080d1f0476a9dcec9678f908d3
Caitrin Armstrong <caitrin.armstrong@mail.mcgill.ca>
2018-10-12 16:33:06 -0400

    updated documentation and added target length check

--- vulcanai2/datasets/multidataset.py
+++ vulcanai2/datasets/multidataset.py
@@ -15,11 +15,15 @@ class MultiDataset(Dataset):
     def __init__(self, datasets):
         """
         Takes in a list of datasets, and whether or not their input_data and target data should be output
-        :param datasets: A list of tuples, where each tuple is in the form
+        :param datasets: A list of tuples, where each tuple is in the form. Can only ever specificy one target.
         (Dataset Object, input_data_boolean, target_data_boolean)
         """
 
         self._datasets = datasets
+
+        # must always have exactly one target.
+        if sum([x[2] for x in self._datasets]) != 1:
+            raise ValueError("You may specify only one target")
 
     def __len__(self):
         """
@@ -30,6 +34,11 @@ class MultiDataset(Dataset):
         return min([t[0].__len__() for t in self._datasets])
 
     def __getitem__(self, idx):
+        """
+        Defines get item used by DataLoader as required by torch.utils.data.Dataset
+        :param idx: index
+        :return: a tuple of input_data and target at that index as specific in config. target data is always last.
+        """
         input_data_items = []
         target_items = []
 
