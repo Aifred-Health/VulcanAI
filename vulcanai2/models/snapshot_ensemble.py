0c0888a03811b79cf5441892bb6ac0d38575b558
rfratila <robertfratila10@gmail.com>
2018-10-13 17:28:25 -0400

    Snapshot training and forward_pass implemented

--- vulcanai2/models/snapshot_ensemble.py
+++ vulcanai2/models/snapshot_ensemble.py
@@ -1,16 +1,17 @@
 
 from copy import deepcopy
+import numpy as np
 
 import torch
 import torch.nn as nn
 from torch.optim.lr_scheduler import CosineAnnealingLR
 
 from .basenetwork import BaseNetwork
+from .metrics import Metrics
 
-class Snapshot(object):
+class SnapshotNet(object):
     """Uses Network to build model snapshots."""
-
-    def __init__(self, name, template_network, n_snapshots):
+    def __init__(self, name, template_network, n_snapshots=3):
         """
         Initialize snapshot ensemble given a network.
 
@@ -20,97 +21,85 @@ class Snapshot(object):
         :param n_epochs: total number of epochs
         """
         self.name = name
-        # self.timestamp = get_timestamp()
         if not isinstance(template_network, BaseNetwork):
-            raise ValueError("template_network must inherit from BaseNetwork.")
-        self.template_network = template_network
+            raise ValueError("template_network type must inherit from BaseNetwork.")
 
-        self.num_classes = template_network.num_classes
+        self.template_network = deepcopy(template_network)
 
         if n_snapshots <= 0:
             raise ValueError("n_snapshots must be >=1.")
-
         self.M = n_snapshots
-
-        self.networks = []
-
-    def cos_annealing(self, alpha, t):
-        """
-        Cosine annealing for fast convergence in snapshot learning.
-        Args:
-            alpha: the old learning rate
-            t: current iteration
-        Returns new learning rate
-        """
-        inner_cos = (np.pi * (t % (self.T // self.M))) / (self.T // self.M)
-        outer_cos = np.cos(inner_cos) + 1
-        return float(alpha / 2 * outer_cos)
+        self.snapshot_networks = {}
 
     def fit(self, train_loader, val_loader, epochs,
             retain_graph=None, valid_interv=4, plot=False):
         """
         Train each model for T/M epochs and sets new network learning rate.
-        Collects each model in a class variable self.networks
+        Collects each model in a class variable self.snapshot_networks
         """
+        
+        # There must be at least one train epoch for each snapshot
         if epochs < self.M:
             print('Number of epochs to small for number of Snapshots. '
-                  'Setting to {}.'.format(self.M))
+                  'Setting epochs to {}.'.format(self.M))
             epochs = self.M
 
-        self.T = epochs # / batch_ratio
-        self.template_network._init_trainer()
-        self.template_network.optim = CosineAnnealingLR(
+        T = epochs
+        # How many epochs each singular network should train for
+        network_epochs = T  // self.M
+        
+        # Temporary but check if it first has an optimizer,
+        # if not it will make one
+        if self.template_network.optim is None:
+            self.template_network._init_trainer()
+
+        self.template_network.lr_scheduler = CosineAnnealingLR(
             optimizer=self.template_network.optim,
-            T_max=epochs
+            T_max=network_epochs
         )
-
-        for i in range(self.M):
-
+        for index in range(self.M):
             self.template_network.fit(
                 train_loader=train_loader,
                 val_loader=val_loader,
-                epochs = self.T  // self.M,
+                epochs = network_epochs,
                 valid_interv=valid_interv,
                 plot=plot
             )
-            self.networks += [deepcopy(self.template_network)]
-        # self.timestamp = get_timestamp()
+            # Save instance of snapshot in a dictionary
+            snap_name = "{}_{}".format(self.name, index)
+            self.snapshot_networks[snap_name] = deepcopy(self.template_network)
 
-    def forward_pass(self, input_data, m=0, convert_to_class=False):
+    def forward_pass(self, data_loader, convert_to_class=False):
         """
-        Get output of ensemble of the last m networks where m <= n_snapshots.
-        Args:
-            input_data: Numpy matrix to make the predictions on
-            m: the m most recent models from the ensemble to give outputs
-               Default to get output from all models.
-            convert_to_class: return class predictions from ensemble
+        Get output of ensemble.
+        :param data_loader: Numpy matrix to make the predictions on
+        :param convert_to_class: return class predictions from ensemble
         """
-        if m < 0 or m > len(self.networks):
-            print('Select the m most recent models to get output from. '
-                  'Setting m to 0 (default to all models)\n')
-            m = 0
-
         prediction_collection = []
-        for net in self.networks[-m:]:
-            prediction_collection += [net.forward_pass(input_data=input_data,
-                                      convert_to_class=False)]
+        for net_key in self.snapshot_networks.keys():
+            print("Getting output from {}".format(net_key))
+            prediction_collection.append(
+                self.snapshot_networks[net_key].forward_pass(
+                    data_loader=data_loader,
+                    convert_to_class=False))
         prediction_collection = np.array(prediction_collection)
         raw_prediction = np.mean(prediction_collection, axis=0,
                                  dtype='float32')
         if convert_to_class:
-            return get_class(raw_prediction)
+            # TODO: be able to use Metrics.get_class outside
+            return Metrics.get_class(None, raw_prediction)
         else:
             return raw_prediction
 
     def save_model(self, save_path='models'):
-        """Save all ensembled networks in a folder with ensemble name."""
+        """Save all ensembled snapshot_networks in a folder with ensemble name."""
         ensemble_path = "{}{}".format(self.timestamp, self.name)
         new_save_path = os.path.join(save_path, ensemble_path)
         if not os.path.exists(new_save_path):
             print ('Creating {} folder'.format(new_save_path))
             os.makedirs(new_save_path)
 
-        for model in self.networks:
+        for model in self.snapshot_networks:
             model.save_model(save_path=new_save_path)
 
         self.save_ensemble_metadata(new_save_path)
@@ -130,12 +119,12 @@ class Snapshot(object):
                 file = os.path.join(ensemble_path, model_file)
                 networks += [Network.load_model(file)]
 
-        snap = Snapshot(
+        snap = SnapshotNet(
             name='snap1',
             template_network=networks[0],
             n_snapshots=config[ensemble_path]['n_snapshots']
         )
-        snap.networks = networks
+        snap.snapshot_networks = networks
         return snap
 
     def save_ensemble_metadata(self, file_path):
