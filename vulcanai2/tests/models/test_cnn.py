3dfddfbd2ff83c1d3e82909bbc508a15fec950d2
rfratila <robertfratila10@gmail.com>
2018-10-19 17:14:46 -0400

    Freeze and unfreeze networks implemented

--- vulcanai2/tests/models/test_cnn.py
+++ vulcanai2/tests/models/test_cnn.py
@@ -1,52 +1,58 @@
+"""Test all ConvNet capabilities."""
 import pytest
 import numpy as np
 import torch
 from vulcanai2.models.cnn import ConvNet
 from torch.utils.data import TensorDataset, DataLoader
 
+
 class TestConvNet:
+    """Define ConvNet test class."""
+
     @pytest.fixture
-    def cnn_noclass(self): 
+    def cnn_noclass(self):
+        """Create ConvNet with no prediction layer."""
         return ConvNet(
             name='Test_ConvNet_noclass',
             dimensions=(1, 28, 28),
             config={
                 'conv_units': [
                     {
-                        "in_channels":1,
-                        "out_channels":16,
-                        "kernel_size":(5, 5),
-                        "stride":2
+                        "in_channels": 1,
+                        "out_channels": 16,
+                        "kernel_size": (5, 5),
+                        "stride": 2
                     },
                     {
-                        "in_channels":16,
-                        "out_channels":1,
-                        "kernel_size":(5, 5),
-                        "stride":1,
-                        "padding":2
+                        "in_channels": 16,
+                        "out_channels": 1,
+                        "kernel_size": (5, 5),
+                        "stride": 1,
+                        "padding": 2
                     }]
             }
         )
 
     @pytest.fixture
     def cnn_class(self):
+        """Create ConvNet with prediction layer."""
         return ConvNet(
             name='Test_ConvNet_class',
             dimensions=(1, 28, 28),
             config={
                 'conv_units': [
                     {
-                        "in_channels":1,
-                        "out_channels":16,
-                        "kernel_size":(5, 5),
-                        "stride":2
+                        "in_channels": 1,
+                        "out_channels": 16,
+                        "kernel_size": (5, 5),
+                        "stride": 2
                     },
                     {
-                        "in_channels":16,
-                        "out_channels":1,
-                        "kernel_size":(5, 5),
-                        "stride":1,
-                        "padding":2
+                        "in_channels": 16,
+                        "out_channels": 1,
+                        "kernel_size": (5, 5),
+                        "stride": 1,
+                        "padding": 2
                     }]
             },
             num_classes=3
@@ -60,7 +66,7 @@ class TestConvNet:
             data_loader=test_dataloader,
             convert_to_class=False)
         assert np.any(~np.isnan(output))
-    
+
     def test_forward_class_not_nan(self, cnn_class):
         """Confirm out is non nan."""
         test_input = torch.ones([1, *cnn_class.in_dim])
@@ -73,3 +79,32 @@ class TestConvNet:
         assert np.any(~np.isnan(class_output))
         assert np.any(~np.isnan(raw_output))
 
+    def test_freeze_class(self, cnn_class):
+        """Test class network freezing."""
+        cnn_class.freeze(apply_inputs=False)
+        for params in cnn_class.network.parameters():
+            assert params.requires_grad is False
+        for params in cnn_class.network_tail.parameters():
+            assert params.requires_grad is False
+
+    def test_unfreeze_class(self, cnn_class):
+        """Test class network unfreezing."""
+        cnn_class.freeze(apply_inputs=False)
+        cnn_class.unfreeze(apply_inputs=False)
+        for params in cnn_class.network.parameters():
+            assert params.requires_grad is True
+        for params in cnn_class.network_tail.parameters():
+            assert params.requires_grad is True
+
+    def test_freeze_noclass(self, cnn_noclass):
+        """Test intermediate network freezing."""
+        cnn_noclass.freeze(apply_inputs=False)
+        for params in cnn_noclass.network.parameters():
+            assert params.requires_grad is False
+
+    def test_unfreeze_noclass(self, cnn_noclass):
+        """Test intermediate network unfreezing."""
+        cnn_noclass.freeze(apply_inputs=False)
+        cnn_noclass.unfreeze(apply_inputs=False)
+        for params in cnn_noclass.network.parameters():
+            assert params.requires_grad is True
--- vulcanai2/tests/models/test_dnn.py
+++ vulcanai2/tests/models/test_dnn.py
@@ -1,12 +1,17 @@
+"""Test all DenseNet capabilities."""
 import pytest
 import numpy as np
 import torch
 from vulcanai2.models.dnn import DenseNet
 from torch.utils.data import TensorDataset, DataLoader
 
+
 class TestDenseNet:
+    """Define DenseNet test class."""
+
     @pytest.fixture
     def dnn_noclass(self):
+        """Create DenseNet with no prediction layer."""
         return DenseNet(
             name='Test_DenseNet_class',
             dimensions=(200),
@@ -15,9 +20,10 @@ class TestDenseNet:
                 'dropout': [0.3, 0.5],
             }
         )
-    
+
     @pytest.fixture
     def dnn_class(self):
+        """Create DenseNet with prediction layer."""
         return DenseNet(
             name='Test_DenseNet_class',
             dimensions=(200),
@@ -36,7 +42,7 @@ class TestDenseNet:
             data_loader=test_dataloader,
             convert_to_class=False)
         assert np.any(~np.isnan(output))
-    
+
     def test_forward_class_not_nan(self, dnn_class):
         """Confirm out is non nan."""
         test_input = torch.ones([5, dnn_class.in_dim])
@@ -49,3 +55,32 @@ class TestDenseNet:
         assert np.any(~np.isnan(raw_output))
         assert np.any(~np.isnan(class_output))
 
+    def test_freeze_class(self, dnn_class):
+        """Test class network freezing."""
+        dnn_class.freeze(apply_inputs=False)
+        for params in dnn_class.network.parameters():
+            assert params.requires_grad is False
+        for params in dnn_class.network_tail.parameters():
+            assert params.requires_grad is False
+
+    def test_unfreeze_class(self, dnn_class):
+        """Test class network unfreezing."""
+        dnn_class.freeze(apply_inputs=False)
+        dnn_class.unfreeze(apply_inputs=False)
+        for params in dnn_class.network.parameters():
+            assert params.requires_grad is True
+        for params in dnn_class.network_tail.parameters():
+            assert params.requires_grad is True
+
+    def test_freeze_noclass(self, dnn_noclass):
+        """Test intermediate network freezing."""
+        dnn_noclass.freeze(apply_inputs=False)
+        for params in dnn_noclass.network.parameters():
+            assert params.requires_grad is False
+
+    def test_unfreeze_noclass(self, dnn_noclass):
+        """Test intermediate network unfreezing."""
+        dnn_noclass.freeze(apply_inputs=False)
+        dnn_noclass.unfreeze(apply_inputs=False)
+        for params in dnn_noclass.network.parameters():
+            assert params.requires_grad is True
