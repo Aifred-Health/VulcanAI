25a0ef5907486351cb9eedafe4c4223914ee2d5a
rfratila <robertfratila10@gmail.com>
2018-10-19 20:06:52 -0400

    Same spacing for __all__ for all __init__ files

--- vulcanai2/datasets/__init__.py
+++ vulcanai2/datasets/__init__.py
@@ -2,6 +2,7 @@
 """ Imports dataset classes so they can be used directly"""
 from .fashion import FashionData
 from .tabulardataset import TabularDataset
+
 __all__ = [
     'fashion',
     'tabulardataset',
