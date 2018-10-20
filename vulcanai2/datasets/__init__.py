9bcc717b505316f7fbf5f534a7a4878aeaddd91f
rfratila <robertfratila10@gmail.com>
2018-10-19 20:05:49 -0400

    Updated datasets __init__ for PEP8

--- vulcanai2/datasets/__init__.py
+++ vulcanai2/datasets/__init__.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 """ Imports dataset classes so they can be used directly"""
+from .fashion import FashionData
+from .tabulardataset import TabularDataset
 __all__ = [
     'fashion',
     'tabulardataset',
-    'utils'
+    'utils',
+    'FashionData',
+    'TabularDataset'
 ]
-from .fashion import FashionData
-from .tabulardataset import TabularDataset
