Fix linear conv conversion by removing the batch from n_channels calculation

examples/fashion_conv_dense_test.py
@@ -211,8 +211,8 @@ conv_very_big = ConvNet(
 
 dense_model = DenseNet(
     name='conv_net_test',
-    input_networks=[conv_small, conv_big, conv_very_big],
-    in_dim=[conv_small.out_dim, conv_big.out_dim, conv_very_big.out_dim],
+    input_networks=None,
+    in_dim=[784],
     config={
     'dense_units': [500, 155],
     'initializer': None,
@@ -220,16 +220,15 @@ dense_model = DenseNet(
     'norm': None,
     'dropout': 0.5,  # Single value or List
     },
-    num_classes=10
 )
 
-dense_output = dense_model(
-    [
-        torch.ones([1,*conv_small.in_dim[0]]),
-        torch.ones([1,*conv_big.in_dim[0]]),
-        torch.ones([1,*conv_very_big.in_dim[0]])
-    ])
-
+# dense_output = dense_model(
+#     [
+#         torch.ones([1,*conv_small.in_dim[0]]),
+#         torch.ones([1,*conv_big.in_dim[0]]),
+#         torch.ones([1,*conv_very_big.in_dim[0]])
+#     ])
+import pudb; pu.db
 model1 = ConvNet(
     name='conv_net_test_multi_input',
     input_networks=[conv_small, dense_model, conv_big, conv_very_big],
@@ -238,16 +237,12 @@ model1 = ConvNet(
     num_classes=10
 )
 
-very_very_big_conv_output = model1.forward(
+very_very_big_conv_output = model1(
     [
-        torch.ones([1,*model1.input_networks[0].in_dim[0]]),
-        [
-            torch.ones([1,*conv_small.in_dim[0]]),
-            torch.ones([1,*conv_big.in_dim[0]]),
-            torch.ones([1,*conv_very_big.in_dim[0]])
-        ],
-        torch.ones([1,*model1.input_networks[2].in_dim[0]]),
-        torch.ones([1,*model1.input_networks[3].in_dim[0]])
+        torch.ones([5,*model1.input_networks[0].in_dim[0]]),
+        torch.ones([5,*model1.input_networks[1].in_dim]),
+        torch.ones([5,*model1.input_networks[2].in_dim[0]]),
+        torch.ones([5,*model1.input_networks[3].in_dim[0]])
     ])
 
 # print(model1.get_output_shapes())