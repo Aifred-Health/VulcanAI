9705fcc7397e088d21bb0865f7a53ee9b3d0d50b
Caitrin Armstrong <caitrin.armstrong@mail.mcgill.ca>
2018-10-28 16:10:53 -0400

    resolve conflicts

---
+++ vulcanai2/tests/datasets/test_tabulardataset.py
@@ -0,0 +1,76 @@
+# coding=utf-8
+""" Defines test cases for tabular dataset """
+import pytest
+import numpy as np
+
+class TestTabularDataset:
+
+    @pytest.fixture
+    def split_data(self):
+        pass
+
+    @pytest.fixture
+    def create_df(self):
+        pass
+        #return df
+
+    def test_dataset_init_single_csv(self):
+        pass
+
+    def test_dataset_init_multiple_csv_correct_merge(self):
+        pass
+
+    def test_dataset_init_multiple_csv_wrong_merge_index(self):
+        pass
+
+    def test_datatset_init(self):
+        pass
+
+    def test_dataset_init(self):
+        pass
+
+    def test_save_dataframe(self):
+        self.fail()
+
+    def test_delete_columns(self):
+        self.fail()
+
+    def test_reverse_create_dummies(self):
+        self.fail()
+
+    def test_list_all_features(self):
+        self.fail()
+
+    def test_list_all_categorical_features(self):
+        self.fail()
+
+    def test_remove_majority_null(self):
+        self.fail()
+
+    def test_remove_unique(self):
+        self.fail()
+
+    def test_remove_unbalanced_columns(self):
+        self.fail()
+
+    def test_remove_highly_correlated(self):
+        self.fail()
+
+    def test_remove_low_variance(self):
+        self.fail()
+
+    def test_split(self):
+        self.fail()
+
+
+
+#define a large fake dataframe
+
+#define 2 large fake csv files
+
+
+#test just supplying a file
+
+#test just supplying a dataframe
+
+#test concatenating dataframes.
