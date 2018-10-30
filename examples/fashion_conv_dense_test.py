simplified network out_dim computation fix #65

examples/fashion_conv_dense_test.py
@@ -246,9 +246,8 @@ x = [
 
 multi = DataLoader(MultiDataset(x), batch_size=100)
 
-# import pudb; pu.db
 # sal_map = compute_saliency_map(model1, x, y)
-# import pudb; pu.db
+
 # display_saliency_overlay(train_loader.dataset.train_data[0], sal_map[0][0])
 
 model1 = ConvNet(
@@ -260,17 +259,17 @@ model1 = ConvNet(
 )
 # a = model1.forward_pass(multi)
 
-dense_model.fit(train_loader, val_loader, 2)
+#model1.fit(multi, multi, 2)
 
 # snap = SnapshotNet("snap", dense_model, 3)
-conv_big.fit(train_loader, val_loader, 3, plot=True)
+#conv_big.fit(train_loader, val_loader, 3, plot=True)
 # snap.save_model()
 #import pudb; pu.db
-model1.save_model()
+#model1.save_model()
 very_very_big_conv_output = model1(
     [
         torch.ones([5,*model1.input_networks[0].in_dim]),
-        torch.ones([5,*conv_small.in_dim]),
+        torch.ones([5,*model1.input_networks[1].in_dim]),
         torch.ones([5,*model1.input_networks[2].in_dim]),
         torch.ones([5,*model1.input_networks[3].in_dim])
     ])

vulcanai2/models/basenetwork.py
@@ -136,15 +136,7 @@ class BaseNetwork(nn.Module):
             pred_activation=pred_activation)
 
         # Compute self.out_dim of the network
-        if self.network is not None:
-            out_shapes = network_summary(
-                network=self.network, input_size=self.in_dim)
-            self.out_dim = tuple(out_shapes[list(out_shapes)[-1]]['output_shape'][1:])
-            
-            if self._num_classes:
-                out_shapes = network_summary(
-                    network=self.network_tail, input_size=self.out_dim)
-                self.out_dim = tuple(out_shapes[list(out_shapes)[-1]]['output_shape'][1:])
+        self.out_dim = self._compute_out_dim()
 
     @abc.abstractmethod
     def _merge_input_network_outputs(self, inputs):
@@ -190,6 +182,13 @@ class BaseNetwork(nn.Module):
             return class_output
         else:
             return network_output
+
+    def _compute_out_dim(self):
+        if self.network is not None:
+            out_dim = self.network(torch.ones([1, *self.in_dim])).shape
+            if self._num_classes:
+                out_dim = self.network_tail(torch.ones(out_dim)).shape
+        return tuple(out_dim)
 
     @property
     def name(self):