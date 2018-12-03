9cb59de495ad39034876c04968437367bb77b358
joemehltretter <mehltret@usc.edu>
2018-12-03 15:45:55 -0800

    Test for selu weight and bias for dense unit

--- vulcanai2/tests/models/test_layers.py
+++ vulcanai2/tests/models/test_layers.py
@@ -1,7 +1,10 @@
 import pytest
 import numpy as np
+import math
+import copy
 import torch
 from vulcanai2.models.layers import *
+from vulcanai2.models.utils import selu_weight_init_, selu_bias_init_
 
 class TestBaseUnit:
     @pytest.fixture
@@ -40,3 +43,49 @@ class TestConvUnit:
         # No padding with 2 5x5 kernels leads from 28x28 -> 24x24
         assert output.size() == torch.ones([1, conv_unit.out_channels, 24, 24]).size()
 
+class TestSeluWeightInit:
+    @pytest.fixture
+    def dense_unit(self):
+        return DenseUnit(in_features=10,
+                         out_features=10
+        )
+
+    def conv_unit(self):
+        return ConvUnit(
+            conv_dim=2,
+            in_channels=10,
+            out_channels=10,
+            kernel_size=(5,5)
+        )
+
+    def test_dense_selu_weight_change(self, dense_unit):
+        starting_weight = copy.deepcopy(dense_unit._kernel.weight)
+        std = math.sqrt(1. / 10)
+        #Produce upper and lower by finding values that are 3 standard deviations from mean
+        # Statistically, ~99% of values in normal distribution are 3 std away from mean.
+        upper = 3 * std
+        lower = 3 * (-(math.sqrt(1. /10)))
+
+
+        dense_unit.weight_init = selu_weight_init_
+        dense_unit.weight_init(dense_unit._kernel.weight)
+        new_weight = copy.deepcopy(dense_unit._kernel.weight)
+        assert ((torch.equal(starting_weight, new_weight) is False) and
+               (new_weight.max().item() <= upper) and
+               (new_weight.min().item() >= lower))
+
+    def test_dense_selu_bias_change(self, dense_unit):
+        starting_bias = copy.deepcopy(dense_unit._kernel.bias)
+        std = math.sqrt(1. / 10)
+        #Produce upper and lower by finding values that are 3 standard deviations from mean
+        # Statistically, ~99% of values in normal distribution are 3 std away from mean.
+        upper = 3 * std
+        lower = 3 * (-(math.sqrt(1. /10)))
+
+
+        dense_unit.bias_init = selu_bias_init_
+        dense_unit.bias_init(dense_unit._kernel.bias)
+        new_bias = copy.deepcopy(dense_unit._kernel.bias)
+        assert ((torch.equal(starting_bias, new_bias) is False) and
+               (new_bias.max().item() <= upper) and
+               (new_bias.min().item() >= lower))
