08f162bd686cce6b08596d49659402bcd797714f
joemehltretter <mehltret@usc.edu>
2018-12-04 13:48:39 -0800

    Update tests to confirm weight and bias are properly initialized for selu

--- vulcanai2/tests/models/test_layers.py
+++ vulcanai2/tests/models/test_layers.py
@@ -61,63 +61,42 @@ class TestSeluWeightInit:
 
     def test_dense_selu_weight_change(self, dense_unit):
         starting_weight = copy.deepcopy(dense_unit._kernel.weight)
-        std = math.sqrt(1. / 10)
-        #Produce upper and lower by finding values that are 3 standard deviations from mean
-        # Statistically, ~99% of values in normal distribution are 3 std away from mean.
-        upper = 3 * std
-        lower = 3 * (-(math.sqrt(1. / 10)))
-
+        std = round(math.sqrt(1. / 10), 1)
 
         dense_unit.weight_init = selu_weight_init_
         dense_unit.weight_init(dense_unit._kernel.weight)
         new_weight = copy.deepcopy(dense_unit._kernel.weight)
-        assert ((torch.equal(starting_weight, new_weight) is False) and
-               (new_weight.max().item() <= upper) and
-               (new_weight.min().item() >= lower))
+        assert (torch.equal(starting_weight, new_weight) is False)
+        assert (round(new_weight.std().item(), 1) == std)
+        assert (round(new_weight.mean().item(), 0) == 0.0)
 
     def test_conv_selu_weight_change(self, conv_unit):
         starting_weight = copy.deepcopy(conv_unit._kernel.weight)
-        std = math.sqrt(1. / 10)
-        #Produce upper and lower by finding values that are 3 standard deviations from mean
-        # Statistically, ~99% of values in normal distribution are 3 std away from mean.
-        upper = 3 * std
-        lower = 3 * (-(math.sqrt(1. / 10)))
+        std = round(math.sqrt(1. / 250), 1)
 
         conv_unit.weight_init = selu_weight_init_
         conv_unit.weight_init(conv_unit._kernel.weight)
         new_weight = copy.deepcopy(conv_unit._kernel.weight)
-        assert ((torch.equal(starting_weight, new_weight) is False) and
-               (new_weight.max().item() <= upper) and
-               (new_weight.min().item() >= lower))
+        assert (torch.equal(starting_weight, new_weight) is False)
+        assert (round(new_weight.std().item(), 1) == std)
+        assert (round(new_weight.mean().item(), 0) == 0.0)
 
     def test_dense_selu_bias_change(self, dense_unit):
         starting_bias = copy.deepcopy(dense_unit._kernel.bias)
-        std = math.sqrt(1. / 10)
-        #Produce upper and lower by finding values that are 3 standard deviations from mean
-        # Statistically, ~99% of values in normal distribution are 3 std away from mean.
-        upper = 3 * std
-        lower = 3 * (-(math.sqrt(1. / 10)))
-
 
         dense_unit.bias_init = selu_bias_init_
         dense_unit.bias_init(dense_unit._kernel.bias)
         new_bias = copy.deepcopy(dense_unit._kernel.bias)
-        assert ((torch.equal(starting_bias, new_bias) is False) and
-               (new_bias.max().item() <= upper) and
-               (new_bias.min().item() >= lower))
+        assert (torch.equal(starting_bias, new_bias) is False)
+        assert (round(new_bias.std().item(), 1) == 0.0)
+        assert (round(new_bias.mean().item(), 0) == 0.0)
 
     def test_conv_selu_bias_change(self, conv_unit):
         starting_bias = copy.deepcopy(conv_unit._kernel.bias)
-        std = math.sqrt(1. / 10)
-        #Produce upper and lower by finding values that are 3 standard deviations from mean
-        # Statistically, ~99% of values in normal distribution are 3 std away from mean.
-        upper = 3 * std
-        lower = 3 * (-(math.sqrt(1. / 10)))
-
 
         conv_unit.bias_init = selu_bias_init_
         conv_unit.bias_init(conv_unit._kernel.bias)
         new_bias = copy.deepcopy(conv_unit._kernel.bias)
-        assert ((torch.equal(starting_bias, new_bias) is False) and
-               (new_bias.max().item() <= upper) and
-               (new_bias.min().item() >= lower))
+        assert (torch.equal(starting_bias, new_bias) is False)
+        assert (round(new_bias.std().item(), 1) == 0.0)
+        assert (round(new_bias.mean().item(), 0) == 0.0)
