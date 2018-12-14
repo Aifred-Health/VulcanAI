c35ad9b8699bdfbbe5c7233dd03d20e99aa725e0
Caitrin Armstrong <caitrin.armstrong@mail.mcgill.ca>
2018-12-13 22:23:54 -0500

    now with dates

--- vulcanai2/__init__.py
+++ vulcanai2/__init__.py
@@ -1,8 +1,10 @@
 import logging, logging.config
 from os import path
+from datetime import datetime
 
 log_file_path = path.join(path.dirname(path.abspath(__file__)), 'log.ini')
-log_output_path = "logfile.log"  # will create a file wherever you're calling
+# will create a file wherever you're calling
+log_output_path = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + "_logfile.log"
 
 logging.config.fileConfig(log_file_path,
                           defaults={'logfilename': log_output_path})
