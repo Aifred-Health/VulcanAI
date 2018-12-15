3c57b467ab603e349e906b73afe261acc43c6848
Priyatharsan Rajasekar <erpriyatharshan@gmail.com>
2018-12-15 02:03:43 -0500

    remove the added network attribute

--- vulcanai2/models/basenetwork.py
+++ vulcanai2/models/basenetwork.py
@@ -491,7 +491,6 @@ class BaseNetwork(nn.Module):
         return criterion_spec
 
     def _init_trainer(self):
-        self._net_spec_initialzed = True
         self.optim = self._init_optimizer(self._optim_spec)
         # TODO: Use logger to describe if the optimizer is changed.
         self.criterion = self._init_criterion(self._criter_spec)
