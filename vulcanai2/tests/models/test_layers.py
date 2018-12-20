68b4b20afd6445c5e16cf589fd816a8528444611
rfratila <robertfratila10@gmail.com>
2018-12-20 18:01:20 -0500

    Update unit parameters and bias initialization

--- vulcanai2/tests/models/test_layers.py
+++ vulcanai2/tests/models/test_layers.py
@@ -1,7 +1,6 @@
 """The script to test all layers and SELU activation properties hold."""
 import pytest
 import math
-import numpy as np
 import copy
 from functools import reduce
 import torch
@@ -23,9 +22,9 @@ class TestBaseUnit:
         return BaseUnit()
 
     def test_init(self, baseunit):
-        """Initialization Test of a BaseUnit object"""
+        """Initialization Test of a BaseUnit object."""
         assert isinstance(baseunit, nn.Sequential)
-        assert hasattr(baseunit, 'initializer')
+        assert hasattr(baseunit, 'weight_init')
         assert hasattr(baseunit, 'bias_init')
         assert hasattr(baseunit, 'norm')
         assert hasattr(baseunit, 'dropout')
@@ -43,51 +42,50 @@ class TestDenseUnit:
         return DenseUnit(
             in_features=10,
             out_features=10,
-            initializer=nn.init.xavier_uniform_,
-            bias_init=0.1,
+            weight_init=nn.init.xavier_uniform_,
+            bias_init=nn.init.zeros_,
             norm='batch',
             activation=nn.ReLU(),
             dropout=0.5
         )
 
     def test_init(self, denseunit):
-        """Initialization Test of a DenseUnit object"""
+        """Initialization Test of a DenseUnit object."""
         assert hasattr(denseunit, 'in_features')
         assert hasattr(denseunit, 'out_features')
         for unit in denseunit.named_children():
-            assert any(unit[0]==i for i in ['_kernel', '_norm', 
+            assert any(unit[0] == i for i in ['_kernel', '_norm', 
                        '_activation', '_dropout'])
             assert isinstance(unit[1], (nn.Linear, 
                               nn.modules.batchnorm._BatchNorm, 
                               nn.ReLU, modules.dropout._DropoutNd))
             assert callable(unit[1])
-    
+
     @pytest.fixture
     def test_denseunit_parameters(self):
-        """Create a dictionary with parameters that 
-        aren't DenseUnit parameters"""
+        """Create a dictionary with incorrect DenseUnit parameters."""
         return dict(
-                    in_channels=10,
-                    out_features=10,
-                    initializer=nn.init.xavier_uniform_,
-                    bias_init=0.1,
-                    norm='batch',
-                    activation=nn.ReLU(),
-                    dropout=0.5
-                )
-    
+            in_channels=10,
+            out_features=10,
+            weight_init=nn.init.xavier_uniform_,
+            bias_init=nn.init.zeros_,
+            norm='batch',
+            activation=nn.ReLU(),
+            dropout=0.5
+        )
+
     def test_create_denseunit(self, test_denseunit_parameters):
-        """Check if passing wrong parameters raises TypeError"""
+        """Check if passing wrong parameters raises TypeError."""
         with pytest.raises(TypeError) as e:
             DenseUnit(**test_denseunit_parameters)
             assert e.message == "__init__() got an unexpected \
                                 keyword argument 'in_channels'"
-  
+
     def test_forward(self, denseunit):
         """Confirm size is expected after forward."""
         test_input = torch.ones([10, denseunit.in_features])
         output = denseunit.forward(test_input)
-        assert output.shape == torch.ones([10, denseunit.out_features]).size()
+        assert output.shape == torch.ones([10, denseunit.out_features]).shape
 
 
 class TestConvUnit:
@@ -100,9 +98,9 @@ class TestConvUnit:
             conv_dim=2,
             in_channels=10,
             out_channels=10,
-            kernel_size=(5,5),
-            initializer=nn.init.xavier_uniform_,
-            bias_init=0.1,
+            kernel_size=(5, 5),
+            weight_init=nn.init.xavier_uniform_,
+            bias_init=nn.init.zeros_,
             norm='batch',
             activation=nn.ReLU(),
             dropout=0.5
@@ -110,44 +108,43 @@ class TestConvUnit:
 
     @pytest.fixture
     def test_convunit_parameters(self):
-        """Create a dictionary with parameters that 
-        aren't ConvUnit parameters"""
+        """Create dictionary with incorrect ConvUnit parameters."""
         return dict(
-                    in_channels=10,
-                    out_features=10,
-                    initializer=nn.init.xavier_uniform_,
-                    bias_init=0.1,
-                    norm='batch',
-                    activation=nn.ReLU(),
-                    dropout=0.5
-                )
-    
+            in_channels=10,
+            out_features=10,
+            weight_init=nn.init.xavier_uniform_,
+            bias_init=nn.init.zeros_,
+            norm='batch',
+            activation=nn.ReLU(),
+            dropout=0.5
+        )
+
     def test_create_convunit(self, test_convunit_parameters):
-        """Check if passing wrong parameters raises TypeError"""
+        """Check if passing wrong parameters raises TypeError."""
         with pytest.raises(TypeError) as e:
             DenseUnit(**test_convunit_parameters)
             assert e.message == "__init__() got an unexpected \
                                 keyword argument 'out_features'"
 
     def test_init(self, convunit):
-        """Initialization Test of a ConvUnit object"""
+        """Initialization Test of a ConvUnit object."""
         assert hasattr(convunit, 'in_channels')
         assert hasattr(convunit, 'out_channels')
         assert hasattr(convunit, 'kernel_size')
         for unit in convunit.named_children():
-            assert any(unit[0]==i for i in ['_kernel', '_norm', 
-                       '_activation', '_dropout'])
+            assert any(unit[0] == i for i in
+                       ['_kernel', '_norm', '_activation', '_dropout'])
             assert isinstance(unit[1], (modules.conv._ConvNd, 
                               nn.modules.batchnorm._BatchNorm, 
                               nn.ReLU, modules.dropout._DropoutNd))
             assert callable(unit[1])
-    
+
     def test_forward(self, convunit):
         """Confirm size is expected after forward."""
         test_input = torch.ones([10, convunit.in_channels, 28, 28])
         output = convunit.forward(test_input)
         # No padding with 2 5x5 kernels leads from 28x28 -> 24x24
-        assert output.shape == torch.ones([10, convunit.out_channels, 24, 24]).size()
+        assert output.shape == torch.ones([10, convunit.out_channels, 24, 24]).shape
 
 
 class TestSeluInit:
