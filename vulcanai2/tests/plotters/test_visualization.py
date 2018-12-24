a68f21114d885969ef37c73296d0f8cfd003686e
joemehltretter <mehltret@usc.edu>
2018-12-24 10:02:27 -0800

    Add tests for display saliency, display pca, display tsne and display confusion matrix

--- vulcanai2/tests/plotters/test_visualization.py
+++ vulcanai2/tests/plotters/test_visualization.py
@@ -1,10 +1,20 @@
 """Will test visualization functions."""
 import pytest
 import numpy as np
+import pandas as pd
 import torch
+from torch.utils.data import TensorDataset, DataLoader
+from vulcanai2.datasets import tabulardataset
+import os
 from copy import deepcopy
-
-from vulcanai2.plotters.visualization import compute_saliency_map
+from sklearn.datasets import load_digits
+from sklearn.metrics import confusion_matrix
+from vulcanai2.plotters.visualization import compute_saliency_map, \
+                                            display_pca, \
+                                            display_tsne, \
+                                            display_receptive_fields, \
+                                            display_saliency_overlay, \
+                                            display_confusion_matrix
 
 
 class TestVisualization:
@@ -51,6 +61,22 @@ class TestVisualization:
             num_classes=3
         )
 
+    @pytest.fixture
+    def dnn_class_two(self):
+        """Create DenseNet with no prediction layer."""
+        from vulcanai2.models.dnn import DenseNet
+        return DenseNet(
+            name='Test_DenseNet_class',
+            in_dim=(150),
+            activation=torch.nn.SELU(),
+            num_classes=150,
+            config={
+                'dense_units': [150],
+                'dropout': [0.3],
+            },
+            optim_spec={'name': 'Adam', 'lr': 0.001}
+        )
+
     def test_compute_saliency_map_cnn(self, cnn_class):
         """Confirm hooks are removed, and gradient shape."""
         test_input_1B = torch.ones([1, *cnn_class.in_dim])
@@ -82,7 +108,6 @@ class TestVisualization:
         """Confirm hooks are removed, and gradient shape."""
         test_input_1B = torch.ones([1, *dnn_class.in_dim])
         test_input_5B = torch.ones([5, *dnn_class.in_dim])
-
         model_copy = deepcopy(dnn_class)
         # Test shape conservation
         dnn_class.freeze(apply_inputs=False)
@@ -104,3 +129,47 @@ class TestVisualization:
 
         # Test hook removal
         assert dnn_class._backward_hooks == model_copy._backward_hooks
+
+    def test_display_saliency_overlay(self, dnn_class):
+        curr_path = str(os.path.dirname(__file__)) + '/'
+        img = np.zeros((25,25))
+        test_input_1B = torch.ones([1, *dnn_class.in_dim])
+        sal_map_1B = np.array(compute_saliency_map(
+            dnn_class,
+            test_input_1B, torch.LongTensor([2])))
+        display_saliency_overlay(img, sal_map_1B, shape=(25,25), save_path=curr_path)
+        for file in os.listdir(curr_path):
+            if file.startswith('saliency'):
+                assert True
+                file_path = curr_path + file
+        os.remove(file_path)
+
+    def test_display_pca(self, dnn_class):
+        curr_path = str(os.path.dirname(__file__)) + '/'
+        digits = load_digits()
+        dnn_pca = display_pca(digits.data, digits.target, save_path=curr_path)
+        for file in os.listdir(curr_path):
+            if file.startswith('PCA'):
+                assert True
+                file_path = curr_path + file
+                os.remove(file_path)
+
+    def test_display_tsne(self, dnn_class):
+        curr_path = str(os.path.dirname(__file__)) + '/'
+        digits = load_digits()
+        dnn_tsne = display_tsne(digits.data, digits.target, save_path=curr_path)
+        for file in os.listdir(curr_path):
+            if file.startswith('t-SNE'):
+                assert True
+                file_path = curr_path + file
+                os.remove(file_path)
+
+    def test_display_confusion_matrix(self):
+        curr_path = str(os.path.dirname(__file__)) + '/'
+        cm = confusion_matrix(y_true=[0,1,1,0,0], y_pred=[1,1,0,1,0])
+        display_confusion_matrix(cm, class_list=[0, 1], save_path=curr_path)
+        for file in os.listdir(curr_path):
+            if file.startswith('confusion'):
+                assert True
+                file_path = curr_path + file
+                os.remove(file_path)
