ff299cac67e35a398ac5313eee5eca456ea33b43
rfratila <robertfratila10@gmail.com>
2019-01-02 22:12:44 -0500

    Update __init__ of core directory

--- vulcanai/__init__.py
+++ vulcanai/__init__.py
@@ -2,6 +2,14 @@ import logging, logging.config
 from os import path
 from datetime import datetime
 
+from . import models, datasets, plotters
+
+__all__ = [
+    'models',
+    'datasets',
+    'plotters'
+]
+
 log_config_path = path.join(path.dirname(path.abspath(__file__)), 'log.ini')
 # will create a file wherever you're calling
 log_output_path = "logfile.log"
