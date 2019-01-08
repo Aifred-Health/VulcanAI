fc139bf7ec9e25e8bb5d1acfe276904b84c1e1ab
Caitrin Armstrong <caitrin.armstrong@mail.mcgill.ca>
2019-01-07 23:16:05 -0500

    reorder

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import sys
 from unittest.mock import MagicMock
 
 
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- Project information -----------------------------------------------------
