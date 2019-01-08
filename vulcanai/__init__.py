81132c252ec3b616b5f96152f1e94631abd1a437
rfratila <robertfratila10@gmail.com>
2019-01-07 22:47:09 -0800

    Restore __init__.py

--- vulcanai/__init__.py
+++ vulcanai/__init__.py
@@ -2,8 +2,6 @@ import logging, logging.config
 from os import path
 from datetime import datetime
 
-from . import datasets, models, plotters
-
 log_config_path = path.join(path.dirname(path.abspath(__file__)), 'log.ini')
 # will create a file wherever you're calling
 log_output_path = "logfile.log"
