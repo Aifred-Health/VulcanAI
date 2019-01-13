3c635dd0015515059344a59ac9d19462d6f0dd93
Priyatharsan Rajasekar <erpriyatharshan@gmail.com>
2019-01-13 02:05:44 -0500

    rever basepplotter back to the prev state

--- vulcanai/plotters/baseplotter.py
+++ vulcanai/plotters/baseplotter.py
@@ -1,7 +1,6 @@
 """
 This file will describe the required components of any class used to provide plotting functionality
 It is quite possible that we do not want this to be an abstract base class, as we do not require any functionality..
-from graphviz import Digraph
 """
 
 
@@ -15,3 +14,7 @@ class Plotter(object):
 
     @abc.abstractmethod
     def display_something(self):
+        """
+        this is just a test.
+        :return:
+        """
