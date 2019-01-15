d046e5794f100afc9c03c9eeb32f6c4b0c84523b
Joseph Mehltretter <mehltrej@ml.aifredhealth.com>
2019-01-15 16:38:06 -0600

    Test update of __init__ in modules

--- vulcanai/datasets/__init__.py
+++ vulcanai/datasets/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """ Imports dataset classes so they can be used directly"""
-from .fashion import FashionData
-from .tabulardataset import TabularDataset
-from .multidataset import MultiDataset
+from fashion import FashionData
+from tabulardataset import TabularDataset
+from multidataset import MultiDataset
 
 __all__ = [
     'fashion',
