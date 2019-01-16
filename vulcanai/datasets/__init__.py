bd101bec4831fb5df55e63b9466f592e42427905
Joseph Mehltretter <mehltret@usc.edu>
2019-01-16 15:21:17 -0600

    Revert "Revert backt to old init"
    This reverts commit e6833f90c1dbf159f3983e6c94836bcfce5ef303.

--- vulcanai/datasets/__init__.py
+++ vulcanai/datasets/__init__.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 """ Imports dataset classes so they can be used directly"""
+import .fashion
 from .fashion import FashionData
+import .tabulardataset
 from .tabulardataset import TabularDataset
+import .multidataset
 from .multidataset import MultiDataset
 
 __all__ = [
--- vulcanai/models/__init__.py
+++ vulcanai/models/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """ Imports network classes so they can be used directly"""
-from .basenetwork import BaseNetwork
-from .cnn import ConvNet, ConvNetConfig
-from .dnn import DenseNet, DenseNetConfig
-from .ensemble import SnapshotNet
-from .metrics import Metrics
-from .layers import BaseUnit, ConvUnit, DenseUnit, FlattenUnit
+from basenetwork import BaseNetwork
+from cnn import ConvNet, ConvNetConfig
+from dnn import DenseNet, DenseNetConfig
+from ensemble import SnapshotNet
+from metrics import Metrics
+from layers import BaseUnit, ConvUnit, DenseUnit, FlattenUnit
 __all__ = [
     'basenetwork',
     'cnn',
